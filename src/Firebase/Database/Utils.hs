3d217a580bd6c22e412a50c9666fbbed728f3a0a
Tomas Patro <tomas.patro@gmail.com>
2020-06-30 15:45:36 +0200

    Refactored everything except main module

--- src/Firebase/Database/Utils.hs
+++ src/Firebase/Database/Utils.hs
@@ -3,25 +3,28 @@
 
 module Firebase.Database.Utils where
 
-import Network.HTTP.Req
 import Web.HttpApiData (ToHttpApiData)
 import Firebase.Database.Types
-import Firebase.Database.Filters (filterParams)
+import Firebase.Database.Filters (filterParams, encodeQueryParam)
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.Text.Encoding as E
 import qualified Data.Text as T
+import qualified Network.HTTP.Simple as S
 
 
--- baseUrl :: T.Text
--- baseUrl = ".firebaseio.com"
+baseUrl :: T.Text
+baseUrl = ".firebaseio.com"
 
--- dbUrl :: T.Text -> T.Text -> Url 'Https
--- dbUrl pId loc = https (pId `T.append` baseUrl) /: (loc `T.append` ".json")
+dbUrl :: T.Text -> DbLocation -> DbURL
+dbUrl pId loc = (E.encodeUtf8 (pId `T.append` baseUrl),
+                 E.encodeUtf8 (loc `T.append` ".json"))
 
--- dbParams :: (QueryParam p, Semigroup p, Monoid p) => Maybe FbAuthToken -> Filter -> p
--- dbParams tok qr = authParam tok <> filterParams qr
+dbParams :: Maybe FbAuthToken -> Filter -> S.Query
+dbParams tok qr = authParam tok ++ filterParams qr
 
--- authParam :: (QueryParam p, Monoid p) => Maybe FbAuthToken -> p
--- authParam Nothing = mempty
--- authParam (Just tok) =
---   case tok of
---     OAuth2 t  -> "access_token" =: t
---     IdToken t -> "auth" =: t
+authParam :: Maybe FbAuthToken -> S.Query
+authParam Nothing = []
+authParam (Just tok) =
+  case tok of
+    OAuth2 t  -> encodeQueryParam "access_token" t
+    IdToken t -> encodeQueryParam "auth" t
